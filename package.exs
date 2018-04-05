b807ca4e025d2471bbdc33a2c7009b940e92e44c
Carlos Brito Lage <carlos@carloslage.net>
2018-04-05 18:52:50 +0100

    woot

---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "cclock",
+  description: "Cluster clock",
+  version: "0.1.0",
+  keywords: ["erlang", "cluster", "clock"],
+  licenses: [[name: "Apache License 2.0"]],
+  maintainers: [
+    [name: "Joaquim Rocha",  email: "jrocha.github@gmailbox.org"],
+    [name: "Carlos Brito Lage",  email: "carlos@carloslage.net"]],
+  repositories: [[github: "aruki-delivery/cclock", branch: "master"]])
